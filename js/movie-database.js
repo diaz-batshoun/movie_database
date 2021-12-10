654cdf6f2c8e7d7523357153f62370492214cc64
Jose Diaz <Diaz.M.Jose68@gmail.com>
2021-12-10 16:55:01 -0600

    added modal to add movie button

--- js/movie-database.js
+++ js/movie-database.js
@@ -1,5 +1,14 @@
 const API_URL = "https://functional-innate-weight.glitch.me/movies"
 
-fetch(API_URL).then(resp => resp.json()).then(data => {
-        console.log(data)
-    }).catch(err => console.error(err));
+// fetch(API_URL).then(resp => resp.json()).then(data => {
+//         console.log(data)
+//     }).catch(err => console.error(err));
+
+let getMovieInfo = () => {
+    return fetch(API_URL).then(resp => resp.json()).catch(err => console.error(err));
+
+
+}
+getMovieInfo().then(data => console.log(data));
+
+
